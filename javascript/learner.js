0e673e7dfa148e31ad0e1c95ff1993c37450ccb1
mahmoudmedhat22 <92884840+mahmoudmedhat22@users.noreply.github.com>
2024-05-15 02:33:37 +0300

    Add files via upload

---
+++ javascript/learner.js
@@ -0,0 +1,53 @@
+
+// push method //
+// let arr =[10,20,30,40,50]
+// let arry=[]
+// for (Element of arr ){
+
+//     arry.push(Element*10)
+
+// }
+// console.log(arry)
+// push method //
+
+// map method //
+// let arr =[10,20,30,40,50]
+// let arry = arr.map(function(Element){
+//     return Element * 10
+// })
+// console.log(arry)
+// map method //
+
+// filter method
+//  let arr =[10,20,30,40,50]
+//  let arry = arr.filter(function(Element){
+//       if(   Element > 35){
+//         return true
+//       }
+//       else{
+//         return false
+//       }
+//         } )
+//          or 
+//           return > 35
+//         console.log(arry)
+// filter method
+
+// reduce method 
+// let arr =[10,20,30,40,50]
+//  let arry = arr.reduce(function(Element ,  after){
+//      return Element + after
+//  })
+//  console.log(arry)
+// reduce method 
+
+//Event
+// function tst (){
+//     document.getElementById("title2").innerHTML="good nah very good"
+// }
+
+
+//eventListener
+// document.getElementById("title").addEventListener("click" , function(){
+//     document.getElementById("title2").innerHTML ='goooood'
+// })
